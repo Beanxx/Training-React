a8b43c404f2a525a7f8beef38323e2637ab04626
Beanxx <tnqls991211@naver.com>
2022-06-14 21:29:52 +0900

    useState 활용2

--- react_anything/src/App.js
+++ react_anything/src/App.js
@@ -1,24 +1,38 @@
 import { useState } from "react";
 
-function App() {
-  const [time, setTime] = useState(1);
+const heavyWork = () => {
+  console.log("Very heavy work!!");
+  return ["춘식이", "영심이"];
+};
 
-  const handleClick = () => {
-    let newTime;
-    if (time >= 12) {
-      newTime = 1;
-    } else {
-      newTime = time + 1;
-    }
-    setTime(newTime);
+function App() {
+  // useState를 사용해서 초기값을 받아올 때 어떤 무거운 일을 해야 한다면
+  // useState의 인자로 콜백함수를 넣어주면 맨 처음 렌더링 될 때만 실행되게 할 수 있음.
+  const [names, setNames] = useState(() => {
+    return heavyWork();
+  });
+  const [input, setInput] = useState("");
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
   };
 
-  console.log("Update!!");
+  const handleUpload = () => {
+    // state를 변경할 때 새로 변경할 state 값이 이전 state 값과 연관되어 있다면
+    // setState() 인자로 새로운 state를 리턴하는 콜백함수를 넣어주는 것이 좋음
+    setNames((prevState) => {
+      console.log("이전 state: ", prevState);
+      return [input, ...prevState];
+    });
+  };
 
   return (
     <div>
-      <span>Time Now: {time}시</span>
-      <button onClick={handleClick}>Update</button>
+      <input type="text" value={input} onChange={handleInputChange} />
+      <button onClick={handleUpload}>Upload</button>
+      {names.map((name, idx) => {
+        return <p key={idx}>{name}</p>;
+      })}
     </div>
   );
 }
