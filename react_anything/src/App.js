fed6b1c0c94cfd3bc9aa148eb556ccd907bb458c
Beanxx <tnqls991211@naver.com>
2022-06-17 01:24:49 +0900

    useRef 적용

--- react_anything/src/App.js
+++ react_anything/src/App.js
@@ -1,16 +1,43 @@
-import React, { useState } from "react";
-import Timer from "./component/Timer";
+import React, { useState, useRef } from "react";
 
 function App() {
-  const [showTimer, setShowTimer] = useState(false);
+  const [count, setCount] = useState(0);
+  const countRef = useRef(0);
+
+  // console.log(countRef); // countRef.current
+  console.log("Rendering...");
+
+  const increaseCountState = () => {
+    setCount(count + 1);
+  };
+
+  const increaseCountRef = () => {
+    countRef.current += 1;
+    console.log("Ref: ", countRef.current);
+  };
 
   return (
     <div>
-      {/* showTimer가 true일 때 <Timer /> 실행 */}
-      {showTimer && <Timer />}
-      <button onClick={() => setShowTimer(!showTimer)}>Toggle Timer</button>
+      <p>State: {count}</p>
+      <p>Ref: {countRef.current}</p>
+      <button onClick={increaseCountState}>State 올려</button>
+      <button onClick={increaseCountRef}>Ref 올려</button>
     </div>
   );
 }
 
 export default App;
+
+// const ref = useRef(value)
+// ref -> { current: value }
+// ref는 수정 가능
+
+// const ref = useRef("hi")
+// { current: "hi" }
+// ref는 state와 비슷하게 어떤 값을 저장해두는 공간 => 저장공간
+
+// state 변화 -> 자동으로 컴포넌트 다시 렌더링 -> 컴포넌트 내부 변수들 초기화
+// ref 변화 -> ref 안의 값을 변경해도 컴포넌트는 다시 렌더링 Nope -> 변수들 값 유지
+// state 변화 -> 렌더링 -> Ref 값은 유지
+
+// Ref로 DOM 요소에 접근 (ex. focus() ...)
