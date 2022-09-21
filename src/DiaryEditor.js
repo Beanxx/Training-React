f27042b048699df53685ae279d61458ecda2b794
Beanxx <tnqls991211@naver.com>
2022-09-22 00:43:10 +0900

    feat: 2개 값을 하나의 변수로 묶어서 상태 관리

---
+++ src/DiaryEditor.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const DiaryEditor = () => {
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+  });
+
+  return (
+    <div className="DiaryEditor">
+      DiaryEditor
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          value={state.author}
+          onChange={(e) => {
+            setState({ ...state, author: e.target.value });
+          }}
+        />
+      </div>
+      <div>
+        <textarea
+          value={state.content}
+          onChange={(e) => {
+            setState({ ...state, content: e.target.value });
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
