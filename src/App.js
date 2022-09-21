1db7918f4f613124851f0ce56c48f99cc418e079
Beanxx <tnqls991211@naver.com>
2022-09-22 01:49:53 +0900

    feat: 데이터 추가

--- src/App.js
+++ src/App.js
@@ -1,43 +1,63 @@
+import React, { useState, useRef } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-const dummyList = [
-  {
-    id: 1,
-    author: "최수빈",
-    content: "하잇1",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "채형원",
-    content: "하잇2",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "김원필",
-    content: "하잇3",
-    emotion: 2,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: "황민현",
-    content: "하잇4",
-    emotion: 1,
-    created_date: new Date().getTime(),
-  },
-];
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "최수빈",
+//     content: "하잇1",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "채형원",
+//     content: "하잇2",
+//     emotion: 3,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "김원필",
+//     content: "하잇3",
+//     emotion: 2,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 4,
+//     author: "황민현",
+//     content: "하잇4",
+//     emotion: 1,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+
+    // 새로 작성한 일기를 맨 위로
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
