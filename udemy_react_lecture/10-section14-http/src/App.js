36d39deeb4a297b89891ec0d728c6c2c2761337c
Beanxx <tnqls991211@naver.com>
2022-11-09 12:28:14 +0900

    [Udemy] async & await 사용하기

--- udemy_react_lecture/10-section14-http/src/App.js
+++ udemy_react_lecture/10-section14-http/src/App.js
@@ -5,25 +5,20 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            // api movieData.~로 받아온 이름을 id, title, openingText ..로 이름을 변환해서 저장해놈
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
 
-        setMovies(transformedMovies);
-      })
-      .catch();
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        // api movieData.~로 받아온 이름을 id, title, openingText ..로 이름을 변환해서 저장해놈
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
   }
 
   return (
