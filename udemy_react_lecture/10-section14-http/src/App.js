ec3232c10941d8db1f08f8019d1c756c7b60db34
Beanxx <tnqls991211@naver.com>
2022-11-09 12:25:16 +0900

    [Udemy] fetch - get 요청 보내기

---
+++ udemy_react_lecture/10-section14-http/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  function fetchMoviesHandler() {
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            // api movieData.~로 받아온 이름을 id, title, openingText ..로 이름을 변환해서 저장해놈
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+
+        setMovies(transformedMovies);
+      })
+      .catch();
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
