03ced244bdfd90a2ef7d8d7844a99a1edbd7ca66
Beanxx <tnqls991211@naver.com>
2022-11-09 13:03:09 +0900

    [Udemy] Loading & http error 처리

--- udemy_react_lecture/10-section14-http/src/App.js
+++ udemy_react_lecture/10-section14-http/src/App.js
@@ -4,22 +4,44 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler() {
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        // api movieData.~로 받아온 이름을 id, title, openingText ..로 이름을 변환해서 저장해놈
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+      // axios에선 오류를 내지만 fetch에선 따로 오류를 잡아내지 않음 => so, 오류를 새롭게 생성해줘서 에러 처리해주기
+      // 파싱 전에 요렇게 에러 처리해줘야 함!
+      if (!response.ok) {
+        throw new Error("Something went wrong!"); // 새로운 에러 생성
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          // api movieData.~로 받아온 이름을 id, title, openingText ..로 이름을 변환해서 저장해놈
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
   }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) content = <MoviesList movies={movies} />;
+  if (error) content = <p>{error}</p>;
+  if (isLoading) content = <p>Loading...</p>;
 
   return (
     <React.Fragment>
@@ -27,7 +49,11 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {content}
+        {/* {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
+        {!isLoading && error && <p>{error}</p>}
+        {isLoading && <p>Loading...</p>} */}
       </section>
     </React.Fragment>
   );
