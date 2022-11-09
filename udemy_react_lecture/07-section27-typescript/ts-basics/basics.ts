7fac2a60107c7a0f8f9e744d948e28abd08e2915
Beanxx <tnqls991211@naver.com>
2022-11-09 12:01:44 +0900

    TS 이론 학습

--- udemy_react_lecture/07-section27-typescript/ts-basics/basics.ts
+++ udemy_react_lecture/07-section27-typescript/ts-basics/basics.ts
@@ -20,6 +20,11 @@ isInstructor = true;
 let hobbies: string[];
 hobbies = ['Sprots', 'Cooking'];
 
+let arr: Array<number> = [1, 2, 3]; // 제네릭 사용
+
+// [Tuple]; 배열 길이 고정, 각 요소 타입 지정
+let arr1: [string, number] = ['hi', 100];
+
 // [Object]
 let person: {
   name: string;
@@ -36,6 +41,17 @@ let people: {
   name: string;
   age: number
 }[];
+
+
+// [Enum]: 특정 값들의 집합
+enum Avengers { Captain, IronMan, Thor }
+let hero: Avengers = Avengers.Captain;
+// let hero: Avengers = Avengers[0]; // Captain
+
+enum Avengerss { Captain = 2, IronMan, Thor }
+// let hero1: Avengers = Avengers[2]; // Captain
+// let hero2: Avengers = Avengers[4]; // Thor
+
 
 // [Type inference(타입 추론)]
 // 문자열 타입을 추론하여 다른 타입이 오면 error -> 가급적 :string 처럼 타입 지정 안하고 타입 추론 사용하는게 좋음~
@@ -62,12 +78,15 @@ function add(a: number, b: number): number {
   return a + b;
 }
 
-function printOutput(value: any) {
+function printOutput(value: any): void {
   console.log(value)
 }
-
 // return문이 없어서 type이 void로 타입 추론됨
 // void: null, undefined와 비슷하지만 항상 함수와 결합해서 사용!
+
+function sum(a: number, b = 100): number {
+  return a + b;
+}
 
 
 // [Generics] => 유연성, 타입 안정성 측면에서 굳~
@@ -91,3 +110,36 @@ const updatedArray = insertAtBeginning(demoArray, -1);  // [-1, 1, 2, 3]
 // 아래 두 case는 같은 코드
 // let numbers: number[] = [1, 2, 3];
 // let numbers: Array<number> = [1, 2, 3];
+
+
+// [Interface]; 상호 간에 정의한 약속/규칙
+interface personAge {
+  age: number;
+  hobby?: string; // 옵션 속성으로 인자로 안 넘겨줘도 에러 발생하지 않음
+}
+
+function logAge(obj: personAge) {
+  console.log(obj.age);
+}
+
+let persons = { name: 'Capt', age: 24 };
+logAge(persons); // 24
+
+
+// [ReadOnly]; 읽기 전용 속성
+interface CraftBeer {
+  readonly brand: string;
+}
+
+let myBeer: CraftBeer = { brand: 'Belgian' }
+// myBeer.brand = 'Korean' => error!
+
+let arr2: ReadonlyArray<number> = [1, 2, 3];
+// arr2[0] == 100; => error!
+
+
+// 인터페이스에 정의하지 않은 속성들을 추가로 사용하고 싶을 때
+interface Craft {
+  brand?: string;
+  [propName: string]: any
+}
