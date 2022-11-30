bf312246bfcee5d2946651c66ac2e47d97a36133
Beanxx <tnqls991211@naver.com>
2022-11-30 17:23:32 +0900

    [Udemy] Learning validation when form is submitted

---
+++ udemy_react_lecture/13-section16-form_input/src/components/SimpleInput.js
@@ -0,0 +1,67 @@
+import React, { useState, useRef, useEffect } from "react";
+
+const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("Name Input is valid!!");
+    }
+  }, [enteredNameIsValid]);
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault(); // 기본 동작인 http 요청 막기 -> 새로고침 방지
+
+    setEnteredNameTouched(true);
+
+    // trim()으로 값 사이에 있는 공백들 모두 제거
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return; // 함수 뒷부분 실행되지 않도록 처리
+    }
+    setEnteredNameIsValid(true);
+
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    setEnteredName(""); // 입력된 값 초기화
+  };
+
+  // 입력값이 건드려진 뒤면서 값이 유효하지 않은 경우 => true (error message 표시해주기)
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          value={enteredName}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+        />
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
